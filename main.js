323aac11095491c0e03d8616299e1258402866f2
Vladyslav <vladyslav.davyda.ki.2021@lpnu.ua>
2024-03-23 00:37:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// doc: https://swiperjs.com/swiper-api#parameters
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
